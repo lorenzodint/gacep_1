9f1b6b7626d020a0000213edc8bd285a6ed35e10
Lorenzo D'intino <64317922+lorenzodint@users.noreply.github.com>
2025-08-27 22:53:16 +0200

    Delete struttura directory

--- struttura/strutturaC2/builder.py
+++
@@ -1,9 +0,0 @@
-from struttura import Builder,generate_setters_for
-from struttura.strutturaC2 import DatiRecordC2Model
-
-
-
-@generate_setters_for(DatiRecordC2Model)
-class RecordC2Builder(Builder[DatiRecordC2Model]):
-    def __init__(self):
-        super().__init__(DatiRecordC2Model)
