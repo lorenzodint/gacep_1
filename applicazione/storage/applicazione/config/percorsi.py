e8155750d8f63fdf38778dade963b99cf38a1956
Lorenzo D'intino <64317922+lorenzodint@users.noreply.github.com>
2025-08-27 17:10:44 +0200

    Add files via upload

---
+++ applicazione/storage/applicazione/config/percorsi.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from pathlib import Path
+from pydantic import BaseModel, Field
+
+
+
+
+def get_storage():
+    return 'applicazione/storage/'
+
---
+++ applicazione/storage/applicazione/views/pages/__init__.py
@@ -0,0 +1,7 @@
+from .login import show
+from .register import show
+from .home import show
+from .altro import show
+from .prova import show
+from .test_1 import show
+from .controllo import show
